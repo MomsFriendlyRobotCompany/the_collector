12d9bb5de356b2509b669f5145e8325818723afb
walchko <walchko@users.noreply.github.com>
2019-04-28 11:39:08 -0400

    testing looks good

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 from the_collector import CircularBuffer
 from collections import namedtuple
-import msgpack
+# import msgpack
 import os
-import time
-from io import BytesIO
+# import time
+# from io import BytesIO
 from the_collector import BagIt
 from the_collector import Json, MsgPack, Pickle
 import json
+import msgpack
+import pickle
 
 
 def bagfile(kind):
@@ -39,8 +41,70 @@ def bagfile(kind):
 
     data = bag.read(fname)
 
+    os.remove(fname)
+
     print(copy)
     print(data)
+
+    assert data == copy
+
+
+def bagfile_rw(kind):
+    bag = BagIt(kind)
+
+    copy = {
+        'test': [],
+        'bob': [],
+        'tom': []
+    }
+
+    d = {'a': 1.2345678}
+    tt = namedtuple('tt','a b c')  # this has issues
+
+    for i in range(10):
+        # t = tt('hello', i, 10*i)
+        t = (1,-1,0.00001,100000,)
+        bag.push('test', d)
+        bag.push('bob', t)
+        bag.push('tom', ('a', i,))
+
+        copy['test'].append(d)
+        copy['bob'].append(t)
+        copy['tom'].append(('a', i,))
+
+    fname = bag.write('bob', timestamp=False)
+
+    # print(">> created:", fname)
+
+    # data = bag.read(fname)
+    if bag.packer.proto == 'json':
+        print("<<< json >>>")
+        with open(fname, 'rb') as fd:
+            data = json.load(fd)
+
+        for key, val in data.items():
+            if isinstance(val[0], list):
+                for i in range(len(val)):
+                    data[key][i] = tuple(data[key][i])
+    elif bag.packer.proto == 'pickle':
+        print("<<< pickle >>>")
+        with open(fname, 'rb') as fd:
+            data = pickle.load(fd)
+    elif bag.packer.proto == 'msgpack':
+        print("<<< msgpack >>>")
+        with open(fname, 'rb') as fd:
+            data = msgpack.unpack(fd, use_list=False, raw=False)
+
+        # The original arrays were turned into tuples
+        for key, val in data.items():
+            data[key] = list(data[key])
+    else:
+        assert False
+
+    os.remove(fname)
+
+    print("Copy\n", copy)
+    print("Data\n", data)
 
     assert data == copy
 
@@ -49,12 +113,24 @@ def test_json():
     bagfile(Json)
 
 
+def test_json_lib():
+    bagfile_rw(Json)
+
+
 def test_msgpack():
     bagfile(MsgPack)
 
 
+def test_msgpack_lib():
+    bagfile_rw(MsgPack)
+
+
 def test_pickle():
     bagfile(Pickle)
+
+
+def test_pickle_lib():
+    bagfile_rw(Pickle)
 
 def test_circularBuff():
     cb_len = 10
