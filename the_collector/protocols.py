883e0619a6c7a9d004fb31a8f2ac8c818ab48c9a
Kevin <walchko@users.noreply.github.com>
2020-04-20 15:35:33 -0400

    Update protocols.py

--- the_collector/protocols.py
+++ the_collector/protocols.py
@@ -81,47 +81,47 @@ class Json:
         raise NotImplementedError("Json.unpacks()")
 
 
-try:
-    import msgpack
+# try:
+#     import msgpack
 
-    class MsgPack:
-        def __init__(self, pack=None, unpack=None):
-            self.proto = "msgpack"
-            self.pack_hook = pack
-            self.unpack_hook = unpack
-            # if pack:
-            #     self.p = msgpack.Packer(default=pack, use_bin_type=True, strict_types=True)
-            # else:
-            #     self.p = msgpack.Packer(use_bin_type=True, strict_types=True)
+#     class MsgPack:
+#         def __init__(self, pack=None, unpack=None):
+#             self.proto = "msgpack"
+#             self.pack_hook = pack
+#             self.unpack_hook = unpack
+#             # if pack:
+#             #     self.p = msgpack.Packer(default=pack, use_bin_type=True, strict_types=True)
+#             # else:
+#             #     self.p = msgpack.Packer(use_bin_type=True, strict_types=True)
 
-            # if unpack:
-            #     unpacker = msgpack.Unpacker(fd, ext_hook=self.unpack, raw=False)
-            # else:
-            #     unpacker = msgpack.Unpacker(fd, raw=False)
+#             # if unpack:
+#             #     unpacker = msgpack.Unpacker(fd, ext_hook=self.unpack, raw=False)
+#             # else:
+#             #     unpacker = msgpack.Unpacker(fd, raw=False)
 
-        def pack(self, data):
-            return msgpack.packb(data, use_bin_type=True)
-            # return msgpack.packb(data, use_bin_type=True, strict_types=True)
+#         def pack(self, data):
+#             return msgpack.packb(data, use_bin_type=True)
+#             # return msgpack.packb(data, use_bin_type=True, strict_types=True)
 
-        def unpack(self, filename):
-            # return msgpack.unpackb(data, use_list=False, raw=False)
-            with open(filename, 'rb') as fd:
-                # data = json.load(fd)
-                data = msgpack.unpack(fd, use_list=False, raw=False)
+#         def unpack(self, filename):
+#             # return msgpack.unpackb(data, use_list=False, raw=False)
+#             with open(filename, 'rb') as fd:
+#                 # data = json.load(fd)
+#                 data = msgpack.unpack(fd, use_list=False, raw=False)
 
-            # The original arrays were turned into tuples
-            for key, val in data.items():
-                data[key] = list(data[key])
+#             # The original arrays were turned into tuples
+#             for key, val in data.items():
+#                 data[key] = list(data[key])
 
-            return data
+#             return data
 
-except ImportError:
-    class MsgPack(object):
-        def __init__(self):
-            raise Exception("WARNING: msgpack not found, install with `pip install msgpack`")
+# except ImportError:
+#     class MsgPack(object):
+#         def __init__(self):
+#             raise Exception("WARNING: msgpack not found, install with `pip install msgpack`")
 
-        def pack(self, data):
-            return None
+#         def pack(self, data):
+#             return None
 
-        def unpack(self, filename):
-            return None
+#         def unpack(self, filename):
+#             return None
